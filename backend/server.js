fef6f0dca1869136e72946a3559dffe9ac652553
Ayush <ayush@hungerbuddy.app>
2026-04-04 01:38:27 +0530

    fix duplicate cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 const { spawn } = require('child_process');
 const path = require('path');
 require('dotenv').config();
